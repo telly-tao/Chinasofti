10d2dcf1d6492490866e6275a52be106e365588d
telly <1021638817@qq.com>
2020-04-20 17:06:33 +0800

    leisurelife controller add

---
+++ leisurelife/src/com/leisurelife/controller/movie/MoviceDetialsController.java
@@ -0,0 +1,25 @@
+package com.leisurelife.controller.movie;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("mmd")
+public class MoviceDetialsController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
